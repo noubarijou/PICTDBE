79e9c07b4aa7572788799e8fe76e2b0a7db0bff3
luizinbrzado <luiztrineves@gmail.com>
2021-12-06 09:19:06 -0300

    Adicionando CORS Policy

--- ProjetoIntegrador/src/main/java/com/pictd/grupo1/ProjetoIntegrador/controller/CategoriaController.java
+++ ProjetoIntegrador/src/main/java/com/pictd/grupo1/ProjetoIntegrador/controller/CategoriaController.java
@@ -5,16 +5,14 @@ import com.pictd.grupo1.ProjetoIntegrador.model.Produto;
 import com.pictd.grupo1.ProjetoIntegrador.service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/categoria")
+@CrossOrigin(origins = "*", allowedHeaders = "")
 public class CategoriaController {
     @Autowired
     private CategoriaService categoriaService;
--- ProjetoIntegrador/src/main/java/com/pictd/grupo1/ProjetoIntegrador/controller/ProdutoController.java
+++ ProjetoIntegrador/src/main/java/com/pictd/grupo1/ProjetoIntegrador/controller/ProdutoController.java
@@ -6,16 +6,14 @@ import com.pictd.grupo1.ProjetoIntegrador.service.CategoriaService;
 import com.pictd.grupo1.ProjetoIntegrador.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping ("/produto")
+@CrossOrigin(origins = "*", allowedHeaders = "")
 public class ProdutoController {
     @Autowired
     private ProdutoService produtoService;
